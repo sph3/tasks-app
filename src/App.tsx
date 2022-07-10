e4657a6c0eff6bd6da3782f2ee8053e852871332
Sophia <sophialealbarros@gmail.com>
2022-07-10 20:59:31 -0300

    feat(header): create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { AddButton } from './components/AddButton';
 import { Greeting } from './components/Greeting';
+import { Header } from './components/Header';
 
 export const App = () => {
   let [flavor, setFlavor] = useState('ctp-latte');
@@ -8,6 +9,7 @@ export const App = () => {
   return (
     <div className={flavor}>
       <div className='bg-ctp-base h-screen'>
+        <Header />
         <Greeting />
         <AddButton />
       </div>
