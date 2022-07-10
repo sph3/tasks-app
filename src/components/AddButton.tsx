88a335353f813d9fa59e1f33c0489acafe9b3537
Sophia <sophialealbarros@gmail.com>
2022-07-10 20:36:59 -0300

    feat(add button): create component

---
+++ src/components/AddButton.tsx
@@ -0,0 +1,9 @@
+import { Plus } from 'phosphor-react';
+
+export const AddButton = () => {
+  return (
+    <div className='bg-ctp-blue rounded-full shadow-lg w-20 h-20 fixed left-1/2 -translate-x-1/2 bottom-5 flex justify-center items-center'>
+      <Plus size={40} color='#fff' />
+    </div>
+  );
+};
