a8e16829db6ca004d612be5e03259c80ce3105c4
Sophia <sophialealbarros@gmail.com>
2022-07-10 20:36:39 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { AddButton } from './components/AddButton';
+
+export const App = () => {
+  let [flavor, setFlavor] = useState('ctp-latte');
+
+  return (
+    <div className={flavor}>
+      <div className='bg-ctp-base'>
+        <AddButton />
+      </div>
+    </div>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  darkMode: 'media',
+  theme: {
+    fontFamily: {
+      sans: 'Roboto, sans-serif',
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@catppuccin/tailwindcss')({
+      prefix: 'ctp',
+      defaultFlavour: 'mocha',
+    }),
+  ],
+};
