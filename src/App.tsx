527db35c4c99dc07e10e2740e69d8dee9bdaad97
Sophia <sophialealbarros@gmail.com>
2022-07-10 20:42:04 -0300

    feat(greeting): create greeting component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AddButton } from './components/AddButton';
+import { Greeting } from './components/Greeting';
 
 export const App = () => {
   let [flavor, setFlavor] = useState('ctp-latte');
@@ -7,6 +8,7 @@ export const App = () => {
   return (
     <div className={flavor}>
       <div className='bg-ctp-base h-screen'>
+        <Greeting />
         <AddButton />
       </div>
     </div>
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,7 @@
+export const Greeting = () => {
+  return (
+    <div>
+      <p className='font-bold text-4xl text-ctp-text'>Hello, World!</p>
+    </div>
+  );
+};
