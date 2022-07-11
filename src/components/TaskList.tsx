8c4b59484eb17f7edbc999863535f7f7d77fd5fc
Sophia <sophialealbarros@gmail.com>
2022-07-10 23:03:03 -0300

    refactor(task list): remove a few placeholder items from the list

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,12 +2,7 @@ import { Task } from './Task';
 
 export const TaskList = () => {
   return (
-    <div className='flex flex-col gap-4'>
-      <Task date={new Date()} text='Placeholder task' />
-      <Task date={new Date()} text='Placeholder task' />
-      <Task date={new Date()} text='Placeholder task' />
-      <Task date={new Date()} text='Placeholder task' />
-      <Task date={new Date()} text='Placeholder task' />
+    <div className='flex flex-col gap-4 scroll-smooth'>
       <Task date={new Date()} text='Placeholder task' />
       <Task date={new Date()} text='Placeholder task' />
       <Task date={new Date()} text='Placeholder task' />
