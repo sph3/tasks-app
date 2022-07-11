5d3ba2d65f585c0a3503395615e80ed3fdf13cc2
Sophia <sophialealbarros@gmail.com>
2022-07-11 17:46:09 -0300

    feat(task): turn tasks into router links

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 type TaskProps = {
   text: string;
   date: Date;
@@ -5,7 +7,7 @@ type TaskProps = {
 
 export const Task = ({ text, date }: TaskProps) => {
   return (
-    <div className='rounded-2xl w-full bg-ctp-surface0 shadow-lg h-16 flex justify-between items-center p-4'>
+    <Link to='/task/id' className='rounded-2xl w-full bg-ctp-surface0 shadow-lg h-16 flex justify-between items-center p-4'>
       <span className='text-ctp-text text-lg'>{text}</span>
       <span className='text-ctp-subtext0 text-base'>
         {date.toLocaleDateString(undefined, {
@@ -17,6 +19,6 @@ export const Task = ({ text, date }: TaskProps) => {
           minute: '2-digit',
         })}
       </span>
-    </div>
+    </Link>
   );
 };
