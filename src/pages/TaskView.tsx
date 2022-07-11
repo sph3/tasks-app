4d1fdb0dd41ea044cf1de8e22b4b87857d32aaf3
Sophia <sophialealbarros@gmail.com>
2022-07-11 17:20:36 -0300

    refactor(task view): decrease text size

--- src/pages/TaskView.tsx
+++ src/pages/TaskView.tsx
@@ -1,4 +1,4 @@
-import { Header } from "../components/Header";
+import { Header } from '../components/Header';
 
 export const TaskView = () => {
   return (
@@ -7,13 +7,16 @@ export const TaskView = () => {
       <div className='p-3 mt-2 flex flex-col gap-4 pb-28'>
         <div>
           <h1 className='text-3xl font-bold text-ctp-text'>Task Title Here</h1>
-          <h2 className='text-xl text-ctp-subtext0'>
+          <h2 className='text-lg text-ctp-subtext0'>
             Created at: 10/07/2022, 23:06
           </h2>
         </div>
         <div>
           <h1 className='font-bold text-2xl text-ctp-text'>Details:</h1>
-          <div className='mt-1 bg-ctp-surface0 rounded-2xl p-3 w-full flex items-center justify-center h-auto min-h-[64px] max-h-80 overflow-y-scroll shadow-md'>
+          <div
+            className='mt-1 bg-ctp-surface0 rounded-2xl p-3 w-full flex items-center justify-center
+            h-auto min-h-[64px] max-h-80 overflow-y-scroll shadow-md'
+          >
             <p className='text-base m-auto'>
               Lorem ipsum dolor, sit amet tinctio odio ullam voluptatem laborum
               quisquam velit adipisci temporibus assumenda nesciunt, vitae ad
