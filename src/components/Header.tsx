2274e65f16a4d5e8c71e23dece38e34c875b535d
Sophia <sophialealbarros@gmail.com>
2022-07-11 17:19:01 -0300

    style(header): wrap className lines

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,10 @@ export const Header = ({ pageTitle }: HeaderProps) => {
   return (
     <div className='flex flex-row justify-between items-center text-center h-16 bg-ctp-mantle flex-wrap px-2'>
       <div className=''>
-        <button className='text-ctp-text bg-ctp-surface0 w-10 h-10 rounded-lg flex text-center items-center justify-center outline-none hover:brightness-90 duration-100 focus:ring-ctp-overlay2 focus:ring-2'>
+        <button
+          className='text-ctp-text bg-ctp-surface0 w-10 h-10 rounded-lg flex text-center
+          items-center justify-center outline-none hover:brightness-90 duration-100 focus:ring-ctp-overlay2 focus:ring-2'
+        >
           <List size={28} />
         </button>
       </div>
@@ -19,7 +22,10 @@ export const Header = ({ pageTitle }: HeaderProps) => {
 
       <div>
         {pageTitle == 'Task View' ? (
-          <button className='text-xl text-ctp-red bg-ctp-red bg-opacity-20 w-10 h-10 rounded-lg flex text-center items-center justify-center outline-none hover:bg-opacity-40 duration-100 focus:ring-ctp-red focus:ring-2'>
+          <button
+            className='text-xl text-ctp-red bg-ctp-red bg-opacity-20 w-10 h-10 rounded-lg flex text-center
+            items-center justify-center outline-none hover:bg-opacity-40 duration-100 focus:ring-ctp-red focus:ring-2'
+          >
             <Trash />
           </button>
         ) : (
