45887df8c0938ad6bbb41723ec6b124b60de95f2
Sophia <sophialealbarros@gmail.com>
2022-07-11 17:28:32 -0300

    feat(header): make icons bold

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { List, Trash } from 'phosphor-react';
+import { ArrowUUpLeft, List, Trash } from 'phosphor-react';
 
 type HeaderProps = {
   pageTitle: 'Your Tasks' | 'Task View';
@@ -9,10 +9,14 @@ export const Header = ({ pageTitle }: HeaderProps) => {
     <div className='flex flex-row justify-between items-center text-center h-16 bg-ctp-mantle flex-wrap px-2'>
       <div className=''>
         <button
-          className='text-ctp-text bg-ctp-surface0 w-10 h-10 rounded-lg flex text-center
+          className='text-xl text-ctp-text bg-ctp-surface0 w-10 h-10 rounded-lg flex text-center
           items-center justify-center outline-none hover:brightness-90 duration-100 focus:ring-ctp-overlay2 focus:ring-2'
         >
-          <List size={28} />
+          {pageTitle === 'Task View' ? (
+            <ArrowUUpLeft weight='bold' />
+          ) : (
+            <List weight='bold' />
+          )}
         </button>
       </div>
 
@@ -21,12 +25,12 @@ export const Header = ({ pageTitle }: HeaderProps) => {
       </div>
 
       <div>
-        {pageTitle == 'Task View' ? (
+        {pageTitle === 'Task View' ? (
           <button
             className='text-xl text-ctp-red bg-ctp-red bg-opacity-20 w-10 h-10 rounded-lg flex text-center
             items-center justify-center outline-none hover:bg-opacity-40 duration-100 focus:ring-ctp-red focus:ring-2'
           >
-            <Trash />
+            <Trash weight='bold' />
           </button>
         ) : (
           ''
