f574f7480e964d0b5bb3045e082539b83d3d0d6a
Sophia <sophialealbarros@gmail.com>
2022-07-10 22:18:09 -0300

    feat(tasks): add creation date to tasks

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,22 @@
 type TaskProps = {
   text: string;
+  date: Date;
 };
 
-export const Task = ({ text }: TaskProps) => {
+export const Task = ({ text, date }: TaskProps) => {
   return (
-    <div className='rounded-2xl w-full bg-ctp-surface0 shadow-lg h-14 text-lg flex items-center p-4'>
-      <span>{text}</span>
+    <div className='rounded-2xl w-full bg-ctp-surface0 shadow-lg h-14 flex justify-between items-center p-4'>
+      <span className='text-ctp-text text-lg'>{text}</span>
+      <span className='text-ctp-subtext0 text-base'>
+        {date.toLocaleDateString(undefined, {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          hour12: false,
+          minute: '2-digit',
+        })}
+      </span>
     </div>
   );
 };
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -3,22 +3,22 @@ import { Task } from './Task';
 export const TaskList = () => {
   return (
     <div className='flex flex-col gap-4'>
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
-      <Task text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
+      <Task date={new Date()} text='Placeholder task' />
     </div>
   );
 };
